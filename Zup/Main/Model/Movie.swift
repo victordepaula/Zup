3842ffd34c535cfc6567cb32b9fd0623d6528212
Victor de Paula <victordepaula@Victors-Mac.local>
2017-11-02 12:02:24 -0700

    frist commit

---
+++ Zup/Main/Model/Movie.swift
@@ -0,0 +1,9 @@
+//
+//  Movie.swift
+//  Zup
+//
+//  Created by Victor de Paula on 01/11/17.
+//  Copyright © 2017 Victor de Paula. All rights reserved.
+//
+
+import Foundation
---
+++ Zup/Main/Model/MoviesShort.swift
@@ -0,0 +1,9 @@
+//
+//  MoviesShort.swift
+//  Zup
+//
+//  Created by Victor de Paula on 02/11/17.
+//  Copyright © 2017 Victor de Paula. All rights reserved.
+//
+
+import Foundation
